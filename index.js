1ff1474b93d69b2469e811295e5471be5a5ccb24
kahneraja <kahneraja@gmail.com>
2016-10-04 16:02:47 -0400

    edit

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+var http = require('http');
+
 console.log("Hello World");
-// 
+
+var url = 'http://kahneraja.com/feed.json';
+
+http.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var response = JSON.parse(body);
+        console.log("Got a response: ", response);
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
